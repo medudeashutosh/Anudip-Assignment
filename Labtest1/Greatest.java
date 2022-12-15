e802202d440ca2ab949720e6d501c09c590536a2
medudeashutosh <113498074+medudeashutosh@users.noreply.github.com>
2022-12-15 16:23:08 +0530

    Add files via upload

---
+++ Labtest1/Greatest.java
@@ -0,0 +1,38 @@
+package Labtest1;
+
+public class Greatest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//	    Enter First Number
+	    int First_number = 11;
+//	    Enter Second Number
+	    int Second_number = 16;
+//	    Enter Third Number
+	    int third_number = 15;
+
+	    int ans = greatest(First_number,Second_number,third_number);
+	    System.out.println("Greatest number is : "+ans);
+
+	}
+	static int greatest(int first,int second,int third){
+        int largest = 0;
+        if(first>second) {
+            if(first>third) {
+                largest = first;
+            }else {
+                largest = third;
+            }
+        } else {
+            if(second>third) {
+                largest = second;
+            } else {
+                largest = third;
+            }
+        }
+        return largest;
+    }
+
+	}
+
+
